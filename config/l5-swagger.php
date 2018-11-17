52f25ffec84111853c738a43c4b8a4436631c0af
Leandro Acquati <leandroacquati@gmail.com>
2018-11-17 12:01:20 -0200

    darkaonline/l5-swagger:5.7.*

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -42,7 +42,16 @@ return [
         |--------------------------------------------------------------------------
          */
         'middleware' => [
-            'api' => [],
+            'api' => [
+                \App\Http\Middleware\EncryptCookies::class,
+                \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+                \Illuminate\Session\Middleware\StartSession::class,
+                \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+                \App\Http\Middleware\VerifyCsrfToken::class,
+                \Illuminate\Routing\Middleware\SubstituteBindings::class,
+                \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+                'auth',
+            ],
             'asset' => [],
             'docs' => [],
             'oauth2_callback' => [],
