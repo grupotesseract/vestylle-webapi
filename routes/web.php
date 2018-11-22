82cec54d46f78134962a1a6dccd4581cb6efdaca
Leandro Acquati <leandroacquati@gmail.com>
2018-11-21 23:04:21 -0200

    infyomlabs/generator-builder

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('generator_builder', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@builder');
+
+Route::get('field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@fieldTemplate');
+
+Route::post('generator_builder/generate', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generate');
