e7d0331e2eed04cb75454ac87212d5362cfb2c51
Leandro Acquati <leandroacquati@gmail.com>
2018-11-22 00:12:46 -0200

    fix sort buttons on datatable

--- resources/views/layouts/datatables_css.blade.php
+++ resources/views/layouts/datatables_css.blade.php
@@ -2,3 +2,11 @@
 <link href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.bootstrap.min.css">
+
+<style>
+
+table.dataTable thead .sorting:after, table.dataTable thead .sorting_asc:after, table.dataTable thead .sorting_desc:after, table.dataTable thead .sorting_asc_disabled:after, table.dataTable thead .sorting_desc_disabled:after {
+    position: static;
+}
+
+</style>
