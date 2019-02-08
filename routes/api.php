18a97750e6d5fbce179d825d5bdfe4692b0ad583
Evandro Carreira <evandro.carreira@gmail.com>
2019-02-08 12:09:49 -0200

    Web Push added; composer update and GCM extension needed

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('/subscriptions', 'SubscriptionController@index');
+Route::post('/subscription', 'SubscriptionController@store');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
