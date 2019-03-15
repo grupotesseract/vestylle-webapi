3a08e27984738adc16b4b68573e8b2dc03ed4a3c
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-03-14 21:49:33 -0300

    Mudando caminho no services

--- config/services.php
+++ config/services.php
@@ -43,7 +43,7 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'login/github/facebook',
+        'redirect' => '/login/github/facebook',
     ],
 
 ];
