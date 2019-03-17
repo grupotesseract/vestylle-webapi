c05ef061b5c4740ee4472c6dde0587949bcf2436
Lucas Gouvêa (K2) <lucas.fim@gmail.com>
2019-03-17 17:57:57 -0300

    164301243 | CRUD de ofertas com datatable traduzida e upload funcionando

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,3 +2,7 @@
     <a href="{!! route('pessoas.index') !!}"><i class="fa fa-edit"></i><span>Pessoas</span></a>
 </li>
 
+<li class="{{ Request::is('ofertas*') ? 'active' : '' }}">
+    <a href="{!! route('ofertas.index') !!}"><i class="fa fa-edit"></i><span>Ofertas</span></a>
+</li>
+
