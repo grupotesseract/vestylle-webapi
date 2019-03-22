864da8c7f21053a288f0983244fcd6bcb202083e
Leandro Acquati <leandroacquati@gmail.com>
2019-03-22 00:52:12 -0300

    barra superior e lateral, correcoes de traducao

--- resources/views/cupons/index.blade.php
+++ resources/views/cupons/index.blade.php
@@ -4,7 +4,7 @@
     <section class="content-header">
         <h1 class="pull-left">Cupons</h1>
         <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('cupons.create') !!}">Adicionar novo Cupom</a>
+           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('cupons.create') !!}">Adicionar Novo Cupom</a>
         </h1>
     </section>
     <div class="content">
