f6dd099ebaac69450347764e3a86b6cdae51a6a2
Lucas Gouvêa (K2) <lucas.fim@gmail.com>
2019-03-21 23:36:15 -0300

    164790073 | traduz botões, corrige labels e ajusta visualização de foto da oferta no detalhamento

--- resources/views/ofertas/show_fields.blade.php
+++ resources/views/ofertas/show_fields.blade.php
@@ -1,36 +1,38 @@
-<!-- Id Field -->
-<div class="form-group">
-    {!! Form::label('id', 'Id:') !!}
-    <p>{!! $oferta->id !!}</p>
-</div>
+<div class="col-md-6">
+    <!-- Id Field -->
+    <div class="form-group">
+        {!! Form::label('id', 'Id:') !!}
+        <p>{!! $oferta->id !!}</p>
+    </div>
 
-<!-- Descricao Oferta Field -->
-<div class="form-group">
-    {!! Form::label('descricao_oferta', 'Descrição da Oferta') !!}
-    <p>{!! $oferta->descricao_oferta !!}</p>
-</div>
+    <!-- Descricao Oferta Field -->
+    <div class="form-group">
+        {!! Form::label('descricao_oferta', 'Descrição da Oferta') !!}
+        <p>{!! $oferta->descricao_oferta !!}</p>
+    </div>
 
-<!-- Texto Oferta Field -->
-<div class="form-group">
-    {!! Form::label('texto_oferta', 'Texto da Oferta') !!}
-    <p>{!! $oferta->texto_oferta !!}</p>
-</div>
+    <!-- Texto Oferta Field -->
+    <div class="form-group">
+        {!! Form::label('texto_oferta', 'Texto da Oferta') !!}
+        <p>{!! $oferta->texto_oferta !!}</p>
+    </div>
 
-<!-- Foto Oferta Field -->
-<div class="form-group">
-    {!! Form::label('foto_oferta', 'Foto da oferta:') !!}
-    <img src="{!! url('storage/' . $oferta->foto_oferta) !!}" alt="oferta">
-</div>
+    <!-- Created At Field -->
+    <div class="form-group">
+        {!! Form::label('created_at', 'Criada em:') !!}
+        <p>{!! $oferta->created_at !!}</p>
+    </div>
 
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', 'Criada em:') !!}
-    <p>{!! $oferta->created_at !!}</p>
-</div>
+    <!-- Updated At Field -->
+    <div class="form-group">
+        {!! Form::label('updated_at', 'Atualizada em:') !!}
+        <p>{!! $oferta->updated_at !!}</p>
+    </div>
 
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', 'Atualizada em:') !!}
-    <p>{!! $oferta->updated_at !!}</p>
 </div>
-
+<div class="col-md-6">
+    <!-- Foto Oferta Field -->
+    <div class="form-group">
+        <img src="{!! url('storage/' . $oferta->foto_oferta) !!}" width="300" height="300" alt="oferta">
+    </div>
+</div>
