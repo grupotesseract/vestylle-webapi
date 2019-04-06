c92cdc61f8f38341e9a5c2bb361e151b1d998b2a
Renato Gomes <renatoo.agomes@gmail.com>
2019-04-05 22:26:06 -0300

    Commit com proteção das rotas e middleware 'pessoaid' pra checar se a pessoa autenticada é a mesma do id da rota

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,31 @@
 <?php
 
+
+/*
+|--------------------------------------------------------------------------
+| API Routes - Rotas acessiveis via autenticação
+|--------------------------------------------------------------------------
+*/
+Route::group(['middleware' => ['auth:api']], function () {
+
+    Route::resource('pessoas', 'PessoaAPIController')->except(['store', 'index', 'destroy']);
+
+    //Get/Toggle Lista de desejos - Aplicando middleware pessoaid pra checar se é a mesma pessoa
+    Route::get('pessoas/{id}/ofertas', 'PessoaAPIController@getOfertas')->middleware('pessoaid');
+    Route::post('pessoas/{id}/ofertas', 'PessoaAPIController@postOfertas')->middleware('pessoaid');
+
+    Route::resource('cupons', 'CuponAPIController')->except(['update', 'destroy','store']);
+    Route::resource('ofertas', 'OfertaAPIController')->except(['update', 'destroy','store']);
+
+});
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes - Rotas Publicas
 |--------------------------------------------------------------------------
 |
-| Login, Store de pessoas, fale conosco,  GET de quase tudo.
+| Login, Store de pessoas, fale conosco, GET de quase tudo.
 |
 */
 
@@ -17,7 +37,7 @@ Route::post('pessoas', 'PessoaAPIController@store');
 Route::post('/login', 'PessoaAPIController@login');
 
 Route::get('login/facebook', 'PessoaAPIController@redirecionaSocial');
-Route::get('login/facebook/callback', 'PessoaAPIController@trataInformacoesSocial');
+//Route::get('login/facebook/callback', 'PessoaAPIController@trataInformacoesSocial');
 
 Route::get('cupons', 'CuponAPIController@index');
 Route::get('cupons/{id}', 'CuponAPIController@show');
@@ -27,22 +47,6 @@ Route::get('lojas', 'LojaAPIController@show');
 Route::get('ofertas', 'OfertaAPIController@index');
 Route::get('ofertas/{id}', 'OfertaAPIController@show');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes - Rotas acessiveis via autenticação
-|--------------------------------------------------------------------------
-*/
-Route::group(['middleware' => ['auth:api']], function () {
 
-    Route::resource('pessoas', 'PessoaAPIController')->except(['store', 'index', 'destroy']);
-
-    //Get e Toggle na Lista de desejos
-    Route::get('pessoas/{id}/ofertas', 'PessoaAPIController@getOfertas');
-    Route::post('pessoas/{id}/ofertas', 'PessoaAPIController@postOfertas');
-
-    Route::resource('cupons', 'CuponAPIController')->except(['update', 'destroy','store']);
-    Route::resource('ofertas', 'OfertaAPIController')->except(['update', 'destroy','store']);
-
-});
 
 
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 /*
 |--------------------------------------------------------------------------
 | ADMIN Routes
@@ -30,14 +31,9 @@ Route::group(['middleware' => ['role:admin']], function () {
     Route::resource('pessoas', 'PessoaController');
     Route::resource('cupons', 'CuponController');
     Route::get('cupons/pessoa/{id}', 'CuponController@getCuponsPessoa');
-
-    Route::resource('pessoas', 'PessoaController');
     Route::resource('faleConoscos', 'FaleConoscoController');
     Route::resource('ofertas', 'OfertaController');
+    Route::resource('lojas', 'LojaController');
 });
 
 
-Route::resource('pessoas', 'PessoaController');
-Route::resource('faleConoscos', 'FaleConoscoController');
-Route::resource('ofertas', 'OfertaController');
-Route::resource('lojas', 'LojaController');
