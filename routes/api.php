b3d8e3e2c4a93de11d37906a1b82dd5573ab7f0a
Renato Gomes <renatoo.agomes@gmail.com>
2019-05-01 18:47:11 -0300

    removendo rotas resource pessoa/cupon para que possam ser acessadas sem auth

--- routes/api.php
+++ routes/api.php
@@ -10,12 +10,17 @@ Route::group(['middleware' => ['auth:api']], function () {
 
     Route::resource('pessoas', 'PessoaAPIController')->except(['store', 'index', 'destroy']);
 
+    Route::get('pessoas/{id}', 'PessoaAPIController@show')->middleware('pessoaid');
+    Route::post('pessoas/{id}', 'PessoaAPIController@update')->middleware('pessoaid');
+
     //Get/Toggle Lista de desejos - Aplicando middleware pessoaid pra checar se é a mesma pessoa
     Route::get('pessoas/{id}/ofertas', 'PessoaAPIController@getOfertas')->middleware('pessoaid');
     Route::post('pessoas/{id}/ofertas', 'PessoaAPIController@postOfertas')->middleware('pessoaid');
 
-    Route::resource('cupons', 'CuponAPIController')->except(['update', 'destroy','store']);
-    Route::resource('ofertas', 'OfertaAPIController')->except(['update', 'destroy','store']);
+    //TODO
+    // # Rota para listar os cupons ativos da pessoa?
+    // # Rota para listar os cupons utilizados da pessoa?
+    // # Rota para listar as infos de 1 cupom dessa pessoa? (para mostrar o ID unico CupomxPessoa)
 
 });
 
@@ -29,22 +34,18 @@ Route::group(['middleware' => ['auth:api']], function () {
 |
 */
 
-//??
+//Rotas para teste da subscription das push's
 Route::get('/subscriptions', 'SubscriptionController@index');
 Route::post('/subscription', 'SubscriptionController@store');
 
 Route::post('pessoas', 'PessoaAPIController@store');
 Route::post('/login', 'PessoaAPIController@login');
-
+Route::post('fale_conoscos', 'FaleConoscoAPIController@store');
 Route::post('login/facebook', 'PessoaAPIController@redirecionaSocial');
-//Route::get('login/facebook/callback', 'PessoaAPIController@trataInformacoesSocial');
 
+Route::get('lojas', 'LojaAPIController@show');
 Route::get('cupons', 'CuponAPIController@index');
 Route::get('cupons/{id}', 'CuponAPIController@show');
-
-Route::post('fale_conoscos', 'FaleConoscoAPIController@store');
-Route::get('lojas', 'LojaAPIController@show');
 Route::get('ofertas', 'OfertaAPIController@index');
 Route::get('ofertas/{id}', 'OfertaAPIController@show');
 
-Route::resource('lojas', 'LojaAPIController');
