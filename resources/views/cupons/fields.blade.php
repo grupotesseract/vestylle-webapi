9e66c41715ec261e87ebd51b882a8ff3f0e2d403
Renato Gomes <renatoo.agomes@gmail.com>
2019-05-15 16:01:21 -0300

    corrigindo merge que voltei os campos de 'primeiro-login'.

--- resources/views/cupons/fields.blade.php
+++ resources/views/cupons/fields.blade.php
@@ -20,12 +20,6 @@
     {!! Form::text('texto_cupom', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Cupom Primeiro Login Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('cupom_primeiro_login', 'Adicionar ao usuário no primeiro login:') !!}
-    {!! Form::checkbox('cupom_primeiro_login', null, $cupon->cupom_primeiro_login ?? false) !!}
-</div>
-
 <!-- Oferta Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('oferta_id', 'Descrição da Oferta:') !!}
