838b06b691831cef2f2fd0800320f000e36018ce
Lucas Gouvêa (K2) <lucas.fim@gmail.com>
2019-05-23 23:10:11 -0300

    feature/166227365 | comando para limpar cupons vencidos e suas relacoes

---
+++ app/Console/Commands/LimparCuponsVencidos.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\RemoverImagem;
+use Illuminate\Console\Command;
+
+class LimparCuponsVencidos extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cupons:remover_cupons_vencidos';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Comando para limpar cupons vencidos a ser executado diariamente';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $cupons_vencidos = \App\Models\Cupon::vencidos()->get(['id'])->all();
+
+        foreach ($cupons_vencidos as $cupom) {
+
+            if ($cupom->fotos) {
+                $this->limparImagensDoCupom($cupom->fotos);
+            }
+
+            \App\Models\CuponPessoa::whereCupomId($cupom->id)->delete();
+
+            $cupom->delete();
+        }
+    }
+
+    /**
+     * Dispara Job de remover imagem para cada foto do cupom a ser excluído
+     *
+     * @return void
+     */
+    public function limparImagensDoCupom($fotos)
+    {
+        foreach ($fotos as $foto) {
+            RemoverImagem::dispatch($foto->id);
+        }
+    }
+}
