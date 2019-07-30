66f8d13736fe00e503ecac27ffd5bb8b6b0e0e22
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-07-30 13:55:59 -0300

    Validador de CPF

---
+++ app/Rules/CPFValido.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class CPFValido implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        // Verifica se um número foi informado
+        if (filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        }
+
+        if (empty($value)) { 
+            return false;
+        }
+        // Elimina possivel mascara
+        $cpf = preg_replace("/[^0-9]/", "", $value);
+        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+        // Verifica se o numero de digitos informados é igual a 11 
+        if (strlen($cpf) != 11) {
+            return false;
+            // Verifica se nenhuma das sequências invalidas abaixo 
+            // foi digitada. Caso afirmativo, retorna falso
+        } else if ($cpf == '00000000000' 
+            || $cpf == '11111111111' 
+            || $cpf == '22222222222' 
+            || $cpf == '33333333333'  
+            || $cpf == '44444444444'  
+            || $cpf == '55555555555'  
+            || $cpf == '66666666666'  
+            || $cpf == '77777777777'  
+            || $cpf == '88888888888'  
+            || $cpf == '99999999999'
+        ) {
+            return false;
+            
+        } else {   
+            // Calcula os digitos verificadores para verificar se o
+            // CPF é válido
+            for ($t = 9; $t < 11; $t++) {
+                
+                for ($d = 0, $c = 0; $c < $t; $c++) {
+                    $d += $cpf{$c} * (($t + 1) - $c);
+                }
+                $d = ((10 * $d) % 11) % 10;
+                if ($cpf{$c} != $d) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'O CPF digitado não é válido';
+    }
+}
