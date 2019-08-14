43aefd12d2947cbb243d931c474eb6524340fed0
Renato Gomes <renatoo.agomes@gmail.com>
2019-08-13 21:36:17 -0300

    Melhorando usabilidade do disparo de notificacoes e da datatable de pessoas que ativaram um cupom

--- resources/views/pessoas/datatables_actions.blade.php
+++ resources/views/pessoas/datatables_actions.blade.php
@@ -1,3 +1,19 @@
+
+{{-- *@OBS*: 'marcar como utilizado no caixa'. Pegando o id do cupon pela URL  --}}
+@if ( isset($mostrarBtnBaixaCaixa) && $mostrarBtnBaixaCaixa )
+    <div class='btn-group'>
+{!! Form::open(['route' => ['cupons.setUtilizadoVenda', \Request::segments()[1]]]) !!}
+    {!! Form::hidden('pessoa_id', $id) !!}
+    {!! Form::button('<i class="glyphicon glyphicon-check"></i>', [
+        'type' => 'submit',
+        'class' => 'btn btn-success btn-xs',
+        'onclick' => "return confirm('Marcar esse cupom como utilizado no caixa?')"
+    ]) !!}
+{!! Form::close() !!}
+    </div>
+
+@else
+
 <div class='btn-group'>
     <a href="{{ route('pessoas.show', $id) }}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-eye-open"></i>
@@ -21,16 +37,6 @@
 
 </div>
 
-{{-- *@OBS*: 'marcar como utilizado no caixa'. Pegando o id do cupon pela URL  --}}
-@if ( isset($mostrarBtnBaixaCaixa) && $mostrarBtnBaixaCaixa )
-<div class='btn-group'>
-    {!! Form::open(['route' => ['cupons.setUtilizadoVenda', \Request::segments()[1]]]) !!}
-    {!! Form::hidden('pessoa_id', $id) !!}
-    {!! Form::button('<i class="glyphicon glyphicon-check"></i>', [
-        'type' => 'submit',
-        'class' => 'btn btn-success btn-xs',
-        'onclick' => "return confirm('Marcar esse cupom como utilizado no caixa?')"
-    ]) !!}
-{!! Form::close() !!}
+
 @endif
-</div>
+
